37fc266f08b8087d2c23678cb966a603129abfd8
Sandeep Bashyal <tsundip4@gmail.com>
2019-10-30 13:22:08 +0545

    stack call

---
+++ stack.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+#include<stdlib.h>
+//#define max 5
+//void push();
+//void pop();
+//void traverse();
+//int top=-1;
+//int st[max];
+//int main()
+//{
+//    int opt;
+//    while(1)
+//        {
+//        printf("choose 1 for push\n");
+//        printf("choose 2 for pop\n");
+//        printf("choose 3 for traverse\n");
+//        printf("choose 0 for exit");
+//        scanf("%d",&opt);
+//        switch(opt)
+//        {
+//    case 1:
+//        {
+//            push();
+//            break;
+//        }
+//    case 2:
+//        {
+//            pop();
+//            break;
+//        }
+//
+//    case 3:
+//        {
+//            traverse();
+//            break;
+//        }
+//      default:
+//        printf("invalid input");
+//    }
+//
+//
+//
+//
+//}
+//}
+//void push()
+//{
+//    int data;
+//    if (top==max-1)
+//    {
+//        printf("stack is full\n");
+//    }
+//    else
+//        {
+//            printf("enter the data ");
+//            scanf("%d",&data);
+//            st[++top]=data;
+//    }
+//
+//}
+//void pop()
+//{
+//    if(top==-1){
+//        printf("no data to pop out\n");
+//    }
+//    else
+//    printf("the popped value is %d\n", st[top--]);
+//}
+//
+//void traverse()
+//{
+//    int i;
+//    if(top==-1){
+//        printf("no data to traverse\n");
+//    }
+//    else
+//        {
+//            for(i=0;i<=top;i++)
+//            {
+//
+//                printf("%d>>",st[i]);
+//            }
+//        }
+//}
+//
+
+
